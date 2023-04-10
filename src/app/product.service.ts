30d0796f3492d31804843daf8671b1c6d711990a
lrey06 <leslynreyes10@gmail.com>
2023-04-10 23:37:07 +0800

    Completed module

---
+++ src/app/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl  = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+}
